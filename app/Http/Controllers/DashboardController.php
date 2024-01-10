7c6bf82fea4415f1ae981c4e50758444134ddf92
KNobHack <akudandia7272@gmail.com>
2024-01-11 02:58:50 +0700

    Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tenda;
+use App\Models\Transaksi;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -9,10 +12,12 @@ class DashboardController extends Controller
     public function index()
     {
         return view('dashboard', [
-            'tenda' => 0,
-            'customers' => 0,
-            'laporan' => 0,
-            'transaksi' => 0,
+            'tenda' => Tenda::count(),
+            'customers' => User::where('role_id', 2)->count(),
+            'transaksi' => Transaksi::count(),
+            'laporan' => Transaksi::whereHas('rental', function ($query) {
+                $query->where('rental_selesai', true);
+            })->count(),
         ]);
     }
 }
